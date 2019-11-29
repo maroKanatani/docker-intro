3b8a9036e558559e46a3e8ae2299e64505715ad3
Kanatani28 <5genbassnohito.maroro0208@gmail.com>
2019-11-29 18:38:20 +0900

    修正

--- dc_sample/app/src/sample.py
+++ dc_sample/app/src/sample.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
  
 @app.route('/')
 def hello():
-    url = 'mysql+pymysql://root:root@localhost:33306/dc_db?charset=utf8'
+    url = 'mysql+pymysql://test:test@0.0.0.0:3306/dc_db?charset=utf8'
     engine = sa.create_engine(url, echo=True)
     rows = engine.execute('SELECT * FROM users')
 
@@ -13,6 +13,10 @@ def hello():
     response = ''.join(names)
 
     return response
+
+@app.route('/hello')
+def hello2():
+    return "Hello"
  
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
